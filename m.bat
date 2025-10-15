0560273bd67cd7368e815ff069f05089d91b0a97
Adel <5flora.adel5@gmail.com>
2025-10-16 00:31:35 +0300

    feat: add Python wrapper for make commands

---
+++ m.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Короткая команда для запуска run.py на Windows
+REM Использование: m <команда> [аргументы]
+REM Примеры: 
+REM   m run
+REM   m git-save "мой коммит"
+REM   m help
+
+python run.py %*
+
